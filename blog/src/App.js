d2be0997d92c49e1c1d4bebc4e461e3a70e03ace
Heemin <viridian135@gmail.com>
2021-10-23 16:45:14 +0900

    add navigation bar

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title,titleUpdate] = useState(['食べログのおすすめ', '写真おすすめ']);
+  let [title2, titleUpdate2] = useState(['Shoping street', 'Shoping cart'])
+  function hey(){
+    return 100;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={{ color: 'blue', fontSize: '30px'}}>開発ブログ</div>
+      </div>
+      <div className="list">
+        <h3>{ title[0] }</h3>
+        <p>ソウルの料理のおすすめ</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ title2[0] }</h3>
+        <p>shopingしたいところ</p>
+        <hr/>
+      </div>
     </div>
   );
 }
